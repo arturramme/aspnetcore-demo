bb2d65f9b7c37f0a9f57e4479deb191f4c35d74c
Artur Ramme <artur.ramme@operacao.rcadigital.com.br>
2019-03-06 15:30:52 -0300

    add swagger

--- aspnetcore-demo.API/Startup.cs
+++ aspnetcore-demo.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace aspnetcore_demo.API
 {
@@ -26,6 +27,16 @@ namespace aspnetcore_demo.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(op =>
+            {
+                op.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "aspnetcore-demo",
+                        Version = "v1",
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +54,13 @@ namespace aspnetcore_demo.API
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "swagger";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "aspnetcore-demo");
+            });
         }
     }
 }
